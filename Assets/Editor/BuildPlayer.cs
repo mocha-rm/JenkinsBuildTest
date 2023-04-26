f1eaf9c0c5452185156d5d16785f5f7ee7ad2ea9
J.H Park <blogerhong@naver.com>
2023-04-26 15:33:56 +0900

    Jenkins Build Setting
    젠킨스 빌드세팅 구축

---
+++ Assets/Editor/BuildPlayer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+
+public class BuildPlayer : MonoBehaviour
+{
+    [MenuItem("Build/Build_AOS")]
+    public static void MyBuild_AOS()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scene1.unity", "Assets/Scene2.unity" };
+        buildPlayerOptions.locationPathName = $"Build/AOS_{PlayerSettings.bundleVersion}.apk";
+        buildPlayerOptions.target = BuildTarget.Android;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build Succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if(summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build Failed");
+        }
+    }
+}
