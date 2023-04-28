b709f71d3131d44f28d099488b804049c434ab62
J.H Park <blogerhong@naver.com>
2023-04-28 14:54:13 +0900

    Add Manager Class
    + Update Square Move Function

---
+++ Assets/GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] RectTransform obj;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            obj.position = new Vector3(0, Screen.height / 2, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            obj.transform.position = new Vector3(Screen.width - obj.rect.width, Screen.height / 2, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            obj.transform.position = new Vector3(Screen.width / 2, Screen.height - obj.rect.height, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            obj.transform.position = new Vector3(Screen.width / 2, 0, 0);
+        }
+    }
+}
